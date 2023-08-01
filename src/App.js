5a2eb62e796463e989660dc803a99b6567c4b749
alexksandrt <83001059+alexksandrt@users.noreply.github.com>
2023-08-01 20:54:35 +0600

    Cart finished

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   const [selectedSize, setSelectedSize] = useState(null);
   const [error, setError] = useState(null);
   const [isCartButtonActive, setIsCartButtonActive] = useState(false);
+  const [cartItems, setCartItems] = useState({}); // Cart items and their quantities
 
   useEffect(() => {
     axios
@@ -33,7 +34,39 @@ function App() {
       setError("Please select a size before adding to cart.");
     } else {
       console.log("Added to cart:", productData.title, "Size:", selectedSize);
+
+      // Update cart items and their quantities
+      setCartItems((prevItems) => {
+        const updatedItems = { ...prevItems };
+        const quantity = updatedItems[selectedSize] || 0;
+        updatedItems[selectedSize] = quantity + 1;
+        return updatedItems;
+      });
     }
+  };
+
+  const handleIncreaseQuantity = (sizeId) => {
+    setCartItems((prevItems) => {
+      const updatedItems = { ...prevItems };
+      const quantity = updatedItems[sizeId] || 0;
+      updatedItems[sizeId] = quantity + 1;
+      return updatedItems;
+    });
+  };
+
+  const handleDecreaseQuantity = (sizeId) => {
+    setCartItems((prevItems) => {
+      const updatedItems = { ...prevItems };
+      const quantity = updatedItems[sizeId] || 0;
+      if (quantity > 0) {
+        updatedItems[sizeId] = quantity - 1;
+      }
+      return updatedItems;
+    });
+  };
+
+  const getTotalItemsCount = () => {
+    return Object.values(cartItems).reduce((total, count) => total + count, 0);
   };
 
   return (
@@ -45,29 +78,46 @@ function App() {
               className={`cart-button ${isCartButtonActive ? 'cart-button-active' : ''}`}
               onClick={handleCartButtonClick}
             >
-              My Cart (<span className="cart-count">3</span>)
+              My Cart (<span className="cart-count">{getTotalItemsCount()}</span>)
             </button>
           </div>
         </div>
         <div className={`menu-container ${isCartButtonActive ? 'item-trans' : ''}`}>
           <div className="menu-content">
             <div className="menu-inside">
-              {productData &&
-                productData.sizeOptions.map((sizeOption) => (
-                  <div className="cart-item" key={sizeOption.id}>
-                    <img src={productData.imageURL} alt="" />
-                    <div className="product-info-preview">
-                      <h2>{productData.title}</h2>
-                      <div className="amount-items">
-                        1x<span>${productData.price.toFixed(2)}</span>
+              {getTotalItemsCount() === 0 ? (
+                <div className="empty-cart-message">Your basket is empty</div>
+              ) : (
+                // Display only items with quantities greater than 0
+                productData &&
+                productData.sizeOptions.map((sizeOption) => {
+                  const sizeLabel = sizeOption.label;
+                  const quantity = cartItems[sizeLabel] || 0;
+                  if (quantity > 0) {
+                    return (
+                      <div className="cart-item" key={sizeOption.id}>
+                        <img src={productData.imageURL} alt="" />
+                        <div className="product-info-preview">
+                          <h2>{productData.title}</h2>
+                          <div className="amount-items">
+                            {quantity}x<span>${(productData.price * quantity).toFixed(2)}</span>
+                          </div>
+                          <div className="size-info-sm">
+                            SIZE:
+                            <div className="size-selected">{sizeOption.label}</div>
+                          </div>
+                        </div>
+                        <div className="quantity-buttons">
+                            <button onClick={() => handleDecreaseQuantity(sizeLabel)}>-</button>
+                            <button onClick={() => handleIncreaseQuantity(sizeLabel)}>+</button>
+                          </div>
                       </div>
-                      <div className="size-info-sm">
-                        SIZE:
-                        <div className="size-selected">{sizeOption.label}</div>
-                      </div>
-                    </div>
-                  </div>
-              ))}
+                    );
+                  } else {
+                    return null; // Hide items with 0 quantity
+                  }
+                })
+              )}
             </div>
           </div>
         </div>
